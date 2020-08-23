c2d2cf3a0b7838aa8678125e2d1c750318eb10a7
Fran0616 <59398607+Fran0616@users.noreply.github.com>
2020-08-23 17:20:41 -0400

    Add files via upload

---
+++ sortMethod.py
@@ -0,0 +1,10 @@
+#sort() method
+#The sort method can sort items on a list in asccending or decending order. 
+#By default it sort items in ascending order
+myList = [8, 10, 6, 2, 4]
+myList.sort()
+print (myList)
+#Sorting list in descending order. 
+myList.sort(reverse=True)
+
+print (myList)
